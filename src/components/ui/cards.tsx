c9d7a7be1891e04d6a2cccba21fef60649e487f7
shyambisht <bishtshyam135@gmail.com>
2024-04-27 19:50:22 +0100

    added arrow ui for card

--- src/components/ui/cards.tsx
+++ src/components/ui/cards.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
+
 interface ImgType {
   dimension: object;
   content_type: string;
@@ -14,19 +17,36 @@ interface CardProps {
 }
 
 const Cards: React.FC<CardProps> = ({ img, desc, date, title }) => {
+  const [onHover, setOnHover] = useState("");
+
+  const handleHoverEnter = () => {
+    setOnHover("translate-x-2 scale-110 transition ease-in-out delay-150");
+  };
+
+  const handleHoverLeave = () => {
+    setOnHover("transition ease-in-out delay-150");
+  };
+
   return (
     <>
-      <section className="h-[22rem] w-[60rem] bg-[#ECE8E1] cursor-pointer hover:bg-slate-300">
-        <div className="flex flex-2 items-center p-10 mt-3 gap-4">
-          <div className="h-full w-[50%] bg-red-500">
+      <section className="h-[22rem] w-[60rem] bg-[#ECE8E1]">
+        <div
+          onMouseEnter={handleHoverEnter}
+          onMouseLeave={handleHoverLeave}
+          className="flex flex-2 items-center p-10 mt-3 gap-4 cursor-pointer "
+        >
+          <div className="h-full w-[50%] bg-red-500 hover:mix-blend-normal">
             <img className="" src={img.url} alt={img.filename} />
           </div>
           <div className="flex flex-col w-[50%] gap-4 p-10">
             <div className="font-mono">{date.toLocaleDateString()}</div>
             <div className="font-semibold">{title}</div>
             <div className="font-normal text-slate-600">{desc}</div>
-            <div>
+            <div className="flex items-center gap-4">
               <button>See Article</button>
+              <span className={`${onHover}`}>
+                <FaArrowRightLong size={18} />
+              </span>
             </div>
           </div>
         </div>
